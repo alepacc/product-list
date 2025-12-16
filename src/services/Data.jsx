5b89805d9a3289506169d3de1c50d46fdd3521a5
alepacc <alessiopacconi@gmail.com>
2025-12-16 18:02:41 +0100

    active state

--- src/services/Data.jsx
+++ src/services/Data.jsx
@@ -4,10 +4,14 @@ function Data() {
   const [data, setData] = useState([]);
 
     useEffect(() => {
-        // fetch("./src/data/data.json") 
+      try { 
         fetch(`${import.meta.env.BASE_URL}data.json`)
+        // fetch("./src/data/data.json") 
         .then(response => response.json())
         .then(data => setData(data));
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
     }, []);
 
     return data;
